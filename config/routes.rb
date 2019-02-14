3be9a34455fe7d760864b85990e1b8787e51894d
Janna De Vos <devosjanna@gmail.com>
2019-02-14 15:08:30 +0800

    Completed pages controller for mylistings and mybookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get "/mybookings", to: "pages#mybookings"
+  get "/mylistings", to: "pages#mylistings"
+
   resources :spaces do
     resources :bookings
   end
