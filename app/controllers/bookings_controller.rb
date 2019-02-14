530cbba7be2ba7b4b675a92185f6d290a38afa3e
Peter Lee <peter.lee230597@gmail.com>
2019-02-14 18:34:05 +0800

    Finished the new and create function in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,27 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Bookings.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
+    @space = Space.find(params[:space_id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.space = Space.find(params[:space_id])
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to mybookings_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +31,13 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking.delete
+    redirect_to mybookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :user_id, :space_id)
   end
 end
