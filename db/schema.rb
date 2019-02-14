1226c7c496603676d03eb90ca42fd73aefb24709
Ashwin Vijendra <ack4and@gmail.com>
2019-02-14 12:01:45 +0800

    added photo to spaces db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_074750) do
+ActiveRecord::Schema.define(version: 2019_02_14_035259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_074750) do
     t.datetime "updated_at", null: false
     t.integer "price"
     t.string "type"
+    t.string "photo"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
