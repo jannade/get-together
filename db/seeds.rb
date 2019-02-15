584889dfc964d4f7885cefe3a4a078f9c34ae0b6
Ashwin Vijendra <ack4and@gmail.com>
2019-02-15 15:57:43 +0800

    display index page with seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,21 @@ sonia = User.create(name: "sonia", email: "sonia@email.com", password: "password
 puts "Users has been created."
 
 puts 'Creating spaces...'
-url = "http://static1.squarespace.com/static/5424db81e4b0d9410165ff00/54506bc3e4b0c5f11fd42a03/545feb7ee4b0a81640041257/1443556759058/HELEN-MILLS-Event-Space.jpg?format=1500w"
-balispace = Space.new(name: "Bali Space", location: "Canggu, Bali, Indonesia", price: 1000000, category: "Family", user: janna)
-balispace.remote_photo_url = url
-balispace.save
+urls = ["https://picsum.photos/200/300?image=1065", "https://picsum.photos/200/300?image=1047", "https://picsum.photos/200/300?image=1040", "https://picsum.photos/200/300?image=979"]
+names = ["Canggu Space", "Berawa Space", "Phukhet Space", "Kuta Space", "Sanur Space", "Nusa Dua Space"]
+locations = ["Canggu", "Berawa", "Phukhet", "Kuta", "Sanur", "Nusa Dua"]
 
-phukhetspace = Space.create(name: "Phuket Space", location: "Chang wat, Phuket, Thailand", price: 5250000, category: "Birthday", user: ashwin)
-phukhetspace.remote_photo_url = url
-phukhetspace.save
+12.times do
+  space = Space.new(name: names.sample, location: locations.sample, price: 25000, category: "Family", user: janna)
+  space.remote_photo_url = urls.sample
+  space.save
+end
 
-berawaspace = Space.create(name: "New Space", location: "Berawa, Bali, Indonesia", price: 890000, category: "Barbeque", user: sonia)
-berawaspace.remote_photo_url = url
-berawaspace.save
-puts 'Spaces has been created'
+puts 'Spaces has been created.'
 
 puts 'Creating bookings...'
-booking1 = Booking.create(date: Date.new(2019,03,03), user: ashwin, space: balispace)
-booking2 = Booking.create(date: Date.new(2019,05,06), user: janna, space: berawaspace)
+booking1 = Booking.create(date: Date.new(2019,03,03), user: ashwin, space: Space.find(1))
+booking2 = Booking.create(date: Date.new(2019,05,06), user: janna, space: Space.find(2))
 puts 'Booking created.'
 
 
