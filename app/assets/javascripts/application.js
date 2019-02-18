1936d67309fad6366a1cbd9fbab1a6c1b72f2936
Janna De Vos <devosjanna@gmail.com>
2019-02-18 16:27:47 +0800

    implemented bootstrap 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,7 @@
 //= require rails-ujs
 //= require_tree .
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,6 +6,7 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery'
+    Popper: ['popper.js', 'default']
   })
 )
 
