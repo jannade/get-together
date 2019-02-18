3edb218f16f866769208059895102f95d81aaddb
Peter Lee <peter.lee230597@gmail.com>
2019-02-18 19:59:57 +0800

    authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # include Pundit
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
