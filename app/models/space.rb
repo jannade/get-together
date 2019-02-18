722eeb416db658d98f13ca470a4a4527ccfcf0f4
Peter Lee <peter.lee230597@gmail.com>
2019-02-18 15:21:18 +0800

    Done with homepage search bar

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,7 +7,7 @@ class Space < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_location?
   include PgSearch
   pg_search_scope :search_by_name_and_location,
-    against: [:name, :location],
+    against: [:name, :location, :category],
 
     using: {
       tsearch: { prefix: true }
